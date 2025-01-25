8aed676bef0260ee75f33589d71a8cadc8593085
steven2k2 <steven.thompson2k24@gmail.com>
2025-01-25 15:45:28 +1100

    feat: added new home page

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import './main.scss';
 
 import template from './template.hbs';
 
-document.body.innerHTML = template({ title: 'Webpack Handlebars Demo', message: 'This is a demo project that combines <b>Webpack</b>, <b>Handlebars</b>, and <b>Bootstrap 5</b> to create a modular, modern web application setup.' });
+document.body.innerHTML = template({ title: 'Webpack Handlebars Demo + SCSS', message: 'This is a demo project that combines <b>Webpack</b>, <b>Handlebars</b>, and <b>Bootstrap 5</b> to create a modular, modern web application setup.' });
